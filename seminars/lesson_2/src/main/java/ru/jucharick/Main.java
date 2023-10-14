f3230cd40361ae3afd15fd5ff7f80a6133f4893f
Julia Ch <juliecha@yandex.ru>
2023-10-14 09:44:44 +0300

    Create lesson_2

---
+++ seminars/lesson_2/src/main/java/ru/jucharick/Main.java
@@ -0,0 +1,10 @@
+import ru.jucharick.client.ClientGUI;
+import ru.jucharick.server.ServerWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        ServerWindow serverWindow = new ServerWindow();
+        new ClientGUI(serverWindow);
+        new ClientGUI(serverWindow);
+    }
+}
---
+++ seminars/lesson_2/src/main/java/ru/jucharick/client/ClientView.java
@@ -0,0 +1,6 @@
+package ru.jucharick.client;
+
+public interface  ClientView {
+    void showMessage(String text);
+    void disconnectFromServer();
+}
