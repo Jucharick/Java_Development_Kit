f8ce4805f5d447f971a5d02c211a8d446d7c5d5a
Julia Ch <juliecha@yandex.ru>
2023-10-22 22:10:16 +0300

    UPD homeWork_3

--- homeWork/lesson_3/src/main/java/ru/jucharick/ex2_compare/CompareArrays.java
+++ homeWork/lesson_3/src/main/java/ru/jucharick/ex2_compare/CompareArrays.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 
 public class CompareArrays {
     public static <T, V> boolean compare(T[] arr, V[] comparedArr){
-        if (!arr.getClass().equals(comparedArr.getClass())) {return false;}
-        if (arr.length != comparedArr.length) {return false;}
+        if (!arr.getClass().equals(comparedArr.getClass())) {return false;} // должны содержать элементы одного типа
+        if (arr.length != comparedArr.length) {return false;} // должны иметь одинаковую длину
         for (int i = 0; i < arr.length; ++i) {
             if (!Objects.equals(arr[i], comparedArr[i])) {
                 return false;
