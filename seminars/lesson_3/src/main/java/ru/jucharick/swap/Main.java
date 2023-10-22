e9e035b1fbc0275c27ec5e257f69ccd3f253544f
Julia Ch <juliecha@yandex.ru>
2023-10-22 14:38:46 +0300

    Add Seminar_3 - Swap

---
+++ seminars/lesson_3/src/main/java/ru/jucharick/swap/Main.java
@@ -0,0 +1,17 @@
+package ru.jucharick.swap;
+
+import java.util.Arrays;
+
+public class Main {
+    private static void swap (Object[] arr, int from, int to){
+        Object temp = arr[from];
+        arr[from] = arr[to];
+        arr[to] = temp;
+    }
+    public static void main(String[] args) {
+        Object[] arr = {1, 2.0,"Hello"};
+        System.out.println(Arrays.toString(arr));
+        swap(arr, 0, 2);
+        System.out.println(Arrays.toString(arr));
+    }
+}
